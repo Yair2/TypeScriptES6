fd9510ba0ba10e06954600a9c95a32e189eb83ec
Yair Melendez <yairmelendez35@gmail.com>
2020-02-26 11:19:50 -0500

    TypeScriptES6

---
+++ app.ts
@@ -0,0 +1,65 @@
+let nombre = "Ricardo Tapia";
+let edad = 23;
+const PERSONAJE = {
+  nombre: nombre,
+  edad: edad
+};
+
+interface Batman {
+    nombre :string ;
+    artesMarciales:String[];
+}
+// Cree una interfaz que sirva para validar el siguiente objeto
+let batman:Batman = {
+    nombre: "Bruno Díaz",
+    artesMarciales: ["Karate","Aikido","Wing Chun","Jiu-Jitsu"]
+  }
+
+
+
+  // Convertir esta funcion a una funcion de flecha
+function resultadoDoble( a:number, b:number ){
+    return (a + b) * 2
+  }
+
+let FunctionFlecha = (a:number, b:number) => ((a+b)*2);
+
+
+
+
+
+// Función con parametros obligatorios, opcionales y por defecto
+// donde NOMBRE = obligatorio
+//       PODER  = opcional
+//       ARMA   = por defecto = "arco"
+function getAvenger( nombre:string, poder?:string, arma:string = 'arco' ){
+    let mensaje:string;
+    if( poder ){
+       mensaje = nombre + " tiene el poder de: " + poder + " y un arma: " + arma;
+       mensaje =`${nombre} tiene el poder de: ${poder} y un arma: ${arma}`;
+    }else{
+       mensaje = nombre + " tiene un " + poder
+       mensaje =`${nombre} tiene un poder ${poder}`;
+    }
+  };
+
+
+   
+// Cree una clase que permita manejar la siguiente estructura
+// La clase se debe de llamar rectangulo,
+// debe de tener dos propiedades:
+//   * base
+//   * altura
+// También un método que calcule el área  =  base * altura,
+// ese método debe de retornar un numero.
+
+
+
+
+class rectangulo {
+    base :number =0;
+    altura :number =0;
+    calArea():number{
+       return  this.base*this.altura;
+    }
+}
